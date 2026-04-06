6288aa149bceca36d5c5cb06e2ada30d13a0c288
Rishik <rishik3555@gmail.com>
2026-04-06 14:55:18 +0530

    feat: add config loader and run grpc server

---
+++ gateway/cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	// cfg := config.LoadConfig()
+
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
---
+++ gateway/internal/config/main.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+)
+
+type Config struct {
+	Port			string
+	UserServiceUrl	string
+}
+
+func LoadConfig() (*Config) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, falling back to system environment variables")
+	}
+	return &Config{
+		Port: getEnv("PORT", "8080"),
+		UserServiceUrl: getEnv("USER_SERVICE_URL", "localhost:50051"),
+	}
+}
+
+func InitFirebase() *firebase.App {
+	credentialsFile := getEnv("FIREBASE_CREDENTIALS", "serviceAccountKey.json")
+	opt := option.WithCredentialsFile(credentialsFile)
+
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err!=nil {
+		log.Fatalf("Failed to initialize Firebase: %v", err)
+	}
+	return app
+}
+
+func getEnv(key string, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
