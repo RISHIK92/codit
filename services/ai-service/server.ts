f2d1739a46e5037b66f514580142f78c7ce359a2
Rishik <rishik3555@gmail.com>
2026-04-17 15:55:02 +0530

    feat: add resource microservice

---
+++ services/ai-service/server.ts
@@ -0,0 +1,44 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import { ReflectionService } from "@grpc/reflection";
+import path from "path";
+import { ResourceProgressServiceService } from "./src/generated/resourceProgress";
+import { resourceProgressHandler } from "./src/handlers/resourceProgressHandler";
+
+const startServer = () => {
+  const server = new grpc.Server();
+
+  server.addService(ResourceProgressServiceService, resourceProgressHandler);
+
+  // Reflection Configuration
+  const PROTO_PATH = path.join(
+    __dirname,
+    "../../../../shared/proto/user.proto",
+  );
+  const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+  const reflection = new ReflectionService(packageDefinition);
+  reflection.addToServer(server);
+
+  const PORT = process.env.PORT || "50052";
+  const URI = `0.0.0.0:${PORT}`;
+
+  server.bindAsync(
+    URI,
+    grpc.ServerCredentials.createInsecure(),
+    (error, port) => {
+      if (error) {
+        console.error(`Failed to bind server: ${error.message}`);
+        return;
+      }
+      console.log(`Node.js User Service running via gRPC on ${URI}`);
+    },
+  );
+};
+
+startServer();
